fb03fffc42d8f5fb2548e9f8f93de53b7f1cbb3a
Juan Martin Escobar <juanma_escobar07@hotmail.com>
2025-04-09 17:11:22 -0300

    agregar titulo

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,19 +6,19 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('principal.php');
+        return view('principal.php', ['titulo' => 'Home']); 
     }
 
     public function nosotros(): string {
-        return view('nosotros.php');
+        return view('nosotros.php', ['titulo' => 'Nosotros']);
     }
     public function comercializacion(): string {
-        return view('comercializacion.php');
+        return view('comercializacion.php', ['titulo' => 'Comercalizacion']);
     }
     public function contacto(): string {
-        return view('contacto.php');
+        return view('contacto.php', ['titulo' => 'Contacto']);
     }
     public function terminos(): string {
-        return view('terminos.php');
+        return view('terminos.php', ['titulo' => 'Terminos y condiciones']);
     }
 }
--- app/Views/header.php
+++ app/Views/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Kicks Up</title>  
+    <title>Kicks Up <?php echo($titulo); ?></title>  
     <link href="assets/css/bootstrap.min.css" rel="stylesheet" 
       integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link href="assets/css/MiEstilo.css" rel="stylesheet">
