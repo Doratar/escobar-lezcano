c787414ac395487769492c6161e24aaddb291560
Juan Martin Escobar <juanma_escobar07@hotmail.com>
2025-04-11 15:11:52 -0300

    redes sociale

--- app/Views/front/footer.php
+++ app/Views/front/footer.php
@@ -1,5 +1,10 @@
 <!-- Inicio de footer -->
 
+<div class="row">
+    <dov class="col">twitter</dov>
+    <dov class="col">instagram</dov>
+    <dov class="col">facebook</dov>
+</div>
 
 </body>
 </html>
