bd4e5d596e407437cfbc3e8f06b96413b186d0a1
Juan Escobar <jescobar@invico.gov.ar>
2025-04-25 10:12:05 -0300

    agregado de categorias

--- app/Views/principal.php
+++ app/Views/principal.php
@@ -1,5 +1,17 @@
+<link rel="stylesheet" href="principal.css">
+
 <!-- Contenido específico de la página -->
 <div class="text-center my-4">
   <h2>Bienvenido a Kicks UP</h2>
-  <p>Aquí va el carrusel</p>
+</div>
+<!-- Categorias -->
+<!-- Calzado -->
+<div class="container">
+  <h2 class="">Categorias</h2>
+  <div class="row">
+    <div class="col"><img src="assets\img\Componente-Gorra.jpg" alt="gorra" class="img-thumbnail" title="Gorras"></div>
+    <div class="col"><img src="assets\img\Componente-Pantalon.jpg" alt="pantalon" class="img-thumbnail"></div>
+    <div class="col"><img src="assets\img\Componente-Remera.jpg" alt="remera" class="img-thumbnail"></div>
+    <div class="col"><img src="assets\img\Componente-Buzo.jpg" alt="buzo" class="img-thumbnail"></div>
+  </div>
 </div>
