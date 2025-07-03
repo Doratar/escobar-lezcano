0db94c316f626091294d091eb8a9c4cd145805f8
Fernando <lezcanofernando18@gmail.com>
2025-07-03 12:07:14 -0300

    subtotal y total

--- app/Views/admin/ventaDetalle.php
+++ app/Views/admin/ventaDetalle.php
@@ -8,10 +8,16 @@
                 <th>Imagen</th>
                 <th>Precio</th>
                 <th>Cantidad</th>
+                <th>Subtotal</th>
             </tr>
         </thead>
         <tbody>
-            <?php foreach($detalles as $detalle): ?>
+            <?php 
+                $total = 0;
+                foreach($detalles as $detalle): 
+                    $subtotal = $detalle['vdetallePrecio'] * $detalle['vdetalleCantidad'];
+                    $total += $subtotal;
+                    ?>
                 <tr>
                     <td><?php echo $detalle['prodId'] ?></td>
                     <td><?php echo $detalle['prodNombre'] ?></td>
@@ -20,8 +26,20 @@
                     </td>
                     <td><?php echo $detalle['vdetallePrecio'] ?></td>
                     <td><?php echo $detalle['vdetalleCantidad'] ?></td>
+                    <td>
+                        $<?php 
+                            $subtotal = $detalle['vdetallePrecio'] * $detalle['vdetalleCantidad'];
+                            echo number_format($subtotal, 2, ',', '.');
+                        ?>
+                    </td>
                 </tr>
             <?php endforeach; ?>
         </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="5" class="text-end fw-bold">Total</td>
+                <td class="fw-bold text-end">$<?php echo number_format($total, 2, ',', '.') ?></td>
+            </tr>
+        </tfoot>
     </table>
 </div>
