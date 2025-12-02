ce8b2ed51b19096334d23f1a135ec315f9d1132d
Juan Martin Escobar <juanma_escobar07@hotmail.com>
2025-12-02 04:39:14 -0300

    mejora del espacio

--- app/Views/front/comercializacion.php
+++ app/Views/front/comercializacion.php
@@ -1,63 +1,64 @@
 <div class="container my-5">
   <section id="comercializacion" class="p-4 rounded bg-light shadow-sm">
-    <h1 class="mb-4">Comercialización</h1>
+    <div class="mx-auto w-50">
+      <h1 class="mb-4">Comercialización</h1>
 
-    <div class="row">
-      <h2 class="h4 mt-3">Tipos de Entregas</h2>
-      <div class="col-md-3">
-        <img src="assets\img\img-pagina\ENVIOS.jpg" class="img-comercializacion" alt="...">
-      </div>
-      <div class="col-md-9">
-        <ul class="list-unstyled ps-3">
+      <div class="row">
+        <h2 class="h4 mt-3">Tipos de Entregas</h2>
+        <div class="col-md-3">
+          <img src="assets\img\img-pagina\ENVIOS.jpg" class="img-comercializacion" alt="...">
+        </div>
+        <div class="col-md-9">
+          <ul class="list-unstyled ps-3">
 
-          <li>• Envíos a domicilio en todo el país.</li>
-          <li>• Retiro en punto de entrega (consultar puntos disponibles).</li>
-          <li>• Entregas express en Corrientes Capital y alrededores (en el día).</li>
-        </ul>
+            <li>• Envíos a domicilio en todo el país.</li>
+            <li>• Retiro en punto de entrega (consultar puntos disponibles).</li>
+            <li>• Entregas express en Corrientes Capital y alrededores (en el día).</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <h2 class="h4 mt-3">Formas de Envío</h2>
+        <div class="col-md-3">
+          <img src="assets\img\img-pagina\CORREO ARGENTINO.png" class="img-comercializacion" alt="...">
+        </div>
+        <div class="col-md-9">
+          <ul class="list-unstyled ps-3">
+            <li>• Correo Argentino – Estándar (3 a 7 días hábiles).</li>
+            <li>• Correo Argentino – Express (1 a 3 días hábiles).</li>
+            <li>• Motomensajería para Corrientes Capital (mismo día si comprás antes de las 12 hs).</li>
+            <li>• Transporte privado a coordinar para compras mayoristas.</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <h2 class="h4 mt-3">Formas de Pago</h2>
+        <div class="col-md-3">
+          <img src="assets\img\img-pagina\MEDIOS DE PAGO.jpg" class="img-comercializacion" alt="...">
+        </div>
+        <div class="col-md-9">
+          <ul class="list-unstyled ps-3">
+            <li>• Tarjetas de crédito y débito (Visa, Mastercard, Naranja, Amex).</li>
+            <li>• MercadoPago (saldo, tarjetas, transferencias).</li>
+            <li>• Transferencia bancaria directa.</li>
+            <li>• Pago en efectivo (Rapipago/PagoFácil mediante código).</li>
+          </ul>
+        </div>
+      </div>
+      <div class="row">
+        <h2 class="h4 mt-3">Información Útil</h2>
+        <div class="col-md-3">
+          <img src="assets\img\img-pagina\INFORMACION UTIL.png" class="img-comercializacion" alt="...">
+        </div>
+        <div class="col-md-9">
+          <ul class="list-unstyled ps-3">
+            <li>• Envíos se despachan dentro de las 24 a 48 hs hábiles luego de la compra.</li>
+            <li>• Recibirás un código de seguimiento al despachar tu pedido.</li>
+            <li>• Todos nuestros productos son 100% originales y nuevos.</li>
+            <li>• Si necesitás factura A, podés solicitarla al momento de la compra.</li>
+          </ul>
+        </div>
       </div>
     </div>
-    <div class="row">
-      <h2 class="h4 mt-3">Formas de Envío</h2>
-      <div class="col-md-3">
-        <img src="assets\img\img-pagina\CORREO ARGENTINO.png" class="img-comercializacion" alt="...">
-      </div>
-      <div class="col-md-9">
-        <ul class="list-unstyled ps-3">
-          <li>• Correo Argentino – Estándar (3 a 7 días hábiles).</li>
-          <li>• Correo Argentino – Express (1 a 3 días hábiles).</li>
-          <li>• Motomensajería para Corrientes Capital (mismo día si comprás antes de las 12 hs).</li>
-          <li>• Transporte privado a coordinar para compras mayoristas.</li>
-        </ul>
-      </div>
-    </div>
-    <div class="row">
-      <h2 class="h4 mt-3">Formas de Pago</h2>
-      <div class="col-md-3">
-        <img src="assets\img\img-pagina\MEDIOS DE PAGO.jpg" class="img-comercializacion" alt="...">
-      </div>
-      <div class="col-md-9">
-        <ul class="list-unstyled ps-3">
-        <li>• Tarjetas de crédito y débito (Visa, Mastercard, Naranja, Amex).</li>
-        <li>• MercadoPago (saldo, tarjetas, transferencias).</li>
-        <li>• Transferencia bancaria directa.</li>
-        <li>• Pago en efectivo (Rapipago/PagoFácil mediante código).</li>
-        </ul>
-      </div>
-    </div>
-    <div class="row">
-      <h2 class="h4 mt-3">Información Útil</h2>
-      <div class="col-md-3">
-        <img src="assets\img\img-pagina\INFORMACION UTIL.png" class="img-comercializacion" alt="...">
-      </div>
-      <div class="col-md-9">
-        <ul class="list-unstyled ps-3">
-        <li>• Envíos se despachan dentro de las 24 a 48 hs hábiles luego de la compra.</li>
-        <li>• Recibirás un código de seguimiento al despachar tu pedido.</li>
-        <li>• Todos nuestros productos son 100% originales y nuevos.</li>
-        <li>• Si necesitás factura A, podés solicitarla al momento de la compra.</li>
-        </ul>
-      </div>
-    </div>
-    <div>
   </section>
 </div>
